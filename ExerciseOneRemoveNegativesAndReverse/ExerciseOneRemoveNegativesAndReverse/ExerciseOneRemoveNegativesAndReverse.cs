77d3e29ec4fb9bb6b4e8fb73de72879bdf64f6b2
Peter Todorov <petodorov@gmail.com>
2016-12-20 17:04:08 +0200

    Sp;ved

---
+++ ExerciseOneRemoveNegativesAndReverse/ExerciseOneRemoveNegativesAndReverse/ExerciseOneRemoveNegativesAndReverse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class ExerciseOneRemoveNegativesAndReverse
+{
+    static void Main(string[] args)
+    {
+
+        int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        List<int> result = new List<int>();
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] >= 0)
+            {
+                result.Add(input[i]);
+            }
+        }
+        result.Reverse();
+        Console.WriteLine(String.Join(" ", result));
+    }
+}
+
