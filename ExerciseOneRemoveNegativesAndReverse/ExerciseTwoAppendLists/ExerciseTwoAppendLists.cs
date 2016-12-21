70f47529646c1e74ac5f6e9e04b0806f08f719c3
Peter Todorov <petodorov@gmail.com>
2016-12-21 16:34:12 +0200

    Solved

---
+++ ExerciseOneRemoveNegativesAndReverse/ExerciseTwoAppendLists/ExerciseTwoAppendLists.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class ExerciseTwoAppendLists
+{
+    static void Main(string[] args)
+    {
+        List<string> input = Console.ReadLine().Split('|').ToList();
+        input.Reverse();
+        List<string> result = new List<string>();
+        foreach (var item in input)
+        {
+           List<string> nums = item.
+                Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            result.AddRange(nums);
+        }
+        Console.WriteLine(String.Join(" ", result));
+    }
+}
+
