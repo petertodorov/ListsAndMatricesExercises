2bd2cac966ab9344c695a5ef067875dd7a5f6959
Peter Todorov <petodorov@gmail.com>
2016-12-31 15:29:52 +0200

    Solved

---
+++ ExerciseSevenCountNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class ExerciseSevenCountNumbers
+{
+    static void Main(string[] args)
+    {
+        List<int> input = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+        input.Sort();
+        int counter = 1;
+        int previousNum = input[0];
+        for (int i = 1; i < input.Count; i++)
+        {
+            if (previousNum == input[i])
+            {
+                counter++;
+            }
+            else
+            {
+                Console.WriteLine($"{previousNum}->{counter}");
+
+                previousNum = input[i];
+                counter = 1;
+            }
+        }
+        Console.WriteLine($"{previousNum}->{counter}");
+    }
+}
